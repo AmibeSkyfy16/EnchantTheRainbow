a843969b8725c7127b490491d51be27da9a90652
skyfy16 <colin.viatte@gmail.com>
2023-02-03 08:09:11 +0100

    tried to update to 1.19.3, but crashing

--- src/main/java/pepjebs/enchant_the_rainbow/EnchantTheRainbowMod.java
+++ src/main/java/pepjebs/enchant_the_rainbow/EnchantTheRainbowMod.java
@@ -1,14 +1,16 @@
 package pepjebs.enchant_the_rainbow;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Rarity;
-import net.minecraft.util.registry.Registry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import pepjebs.enchant_the_rainbow.item.NetherStarFragmentItem;
 
 public class EnchantTheRainbowMod implements ModInitializer {
 
@@ -16,14 +18,11 @@ public class EnchantTheRainbowMod implements ModInitializer {
     public static Logger LOGGER = LogManager.getLogger(MOD_ID);
     public static final String GLINT_COLOR_NBT_TAG = MOD_ID + ":GlintColor";
 
-    public static Item NETHER_STAR_FRAGMENT = null;
+    public static Item NETHER_STAR_FRAGMENT = new Item(new FabricItemSettings());
 
     @Override
     public void onInitialize() {
-        Registry.register(
-                Registry.ITEM,
-                new Identifier(MOD_ID, "nether_star_fragment"),
-                new NetherStarFragmentItem((new Item.Settings()).group(ItemGroup.MISC).rarity(Rarity.UNCOMMON)));
-        NETHER_STAR_FRAGMENT = Registry.ITEM.get(new Identifier(MOD_ID, "nether_star_fragment"));
+        Registry.register(Registries.ITEM, new Identifier(MOD_ID, "nether_star_fragment"), new Item(new FabricItemSettings().rarity(Rarity.UNCOMMON)));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> entries.add(NETHER_STAR_FRAGMENT));
     }
 }
